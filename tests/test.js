0f561029c0f8b55d5fd3fb7a1b9d0a0bd00fc03b
promilo <milind.shah@mail.utoronto.ca>
2017-11-08 21:29:00 -0500

    got nightwatch to work

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,11 @@
 // Your code here ...
 
-module.exports = {};
+module.exports = {
+  'Home test': browser => {
+    browser
+      .url(browser.launchUrl)
+      .waitForElementVisible('#pageIndex', 1000)
+      .assert.containsText('h1', 'New User')
+      .end();
+  }
+};
